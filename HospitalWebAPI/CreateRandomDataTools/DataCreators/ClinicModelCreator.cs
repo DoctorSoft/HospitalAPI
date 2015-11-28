5f730165163ebc77583a1c4c675af7fb32c1c721
DoctorSoft <megopixel91@gmail.com>
2015-11-29 00:06:48 +0300

    Add HospitalModelCreatorTest. Reworked DataCreators classes.

--- HospitalWebAPI/CreateRandomDataTools/DataCreators/ClinicModelCreator.cs
+++ HospitalWebAPI/CreateRandomDataTools/DataCreators/ClinicModelCreator.cs
@@ -8,7 +8,24 @@ namespace CreateRandomDataTools.DataCreators
     {
         public IEnumerable<ClinicStorageModel> GetList()
         {
-           return null; 
+            var firstClinic = new ClinicStorageModel
+            {
+                IsBlocked = false,
+                Id = 0,
+                Name = "Clinic 1",
+                Address = "Address Clinic 1"
+            };
+
+            return new List<ClinicStorageModel>
+            {
+                firstClinic
+            };
         }
     }
 }
+
+/*
+ * Гродненская центральная городская поликлиника - г. Гродно, ул. Транспортная, 3
+ * Городская поликлиника №1 - г. Гродно, ул. Лермонтова, 13
+ * Городская поликлиника №3 - г. Гродно, ул. Пестрака, 4
+*/
