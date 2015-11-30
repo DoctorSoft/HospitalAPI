1371ce7710eef491272d59877462f65acd0c583f
DoctorSoft <megopixel91@gmail.com>
2015-11-30 22:55:56 +0300

    add new test for CreateRandomData

--- HospitalWebAPI/Tests/CreateRandomDataTest/CheckForNulllModelCreatorsTest.cs
+++ HospitalWebAPI/Tests/CreateRandomDataTest/CheckForNulllModelCreatorsTest.cs
@@ -7,39 +7,88 @@ namespace Tests.CreateRandomDataTest
     public class CheckForNulllModelCreatorsTest
     {
         [TestMethod]
+        public void ReturnAdministratorsList()
+        {
+            var administrator = new AdministratorModelCreator();
+            var resoultList = administrator.GetList();
+            Assert.IsNotNull(resoultList);
+        }
+        [TestMethod]
+        public void ReturnClinicBotsList()
+        {
+            var clinicBot = new ClinicBotModelCreator();
+            var resoultList = clinicBot.GetList();
+            Assert.IsNotNull(resoultList);
+        }
+        [TestMethod]
         public void ReturnClinicsList()
         {
             var clinic = new ClinicModelCreator();
-            var clinicsList = clinic.GetList();
-            Assert.IsNotNull(clinicsList);
+            var resoultList = clinic.GetList();
+            Assert.IsNotNull(resoultList);
+        }
+        [TestMethod]
+        public void ReturnClinicUsersList()
+        {
+            var clinicUser = new ClinicUserModelCreator();
+            var resoultList = clinicUser.GetList();
+            Assert.IsNotNull(resoultList);
         }
         [TestMethod]
         public void ReturnFunctionalGroupsList()
         {
             var modelCreator = new FunctionalGroupModelCreator();
-            var models = modelCreator.GetList();
-            Assert.IsNotNull(models);
+            var resoultList = modelCreator.GetList();
+            Assert.IsNotNull(resoultList);
         }
         [TestMethod]
         public void ReturnFunctionsList()
         {
             var function = new FunctionModelCreator();
-            var functionsList = function.GetList();
-            Assert.IsNotNull(functionsList);
+            var resoultList = function.GetList();
+            Assert.IsNotNull(resoultList);
+        }
+        [TestMethod]
+        public void ReturnHospitalBotsList()
+        {
+            var hospitalBot = new HospitalBotModelCreator();
+            var resoultList = hospitalBot.GetList();
+            Assert.IsNotNull(resoultList);
         }
         [TestMethod]
         public void ReturnHospitalsList()
         {
             var hospital = new HospitalModelCreator();
-            var hospitalsList = hospital.GetList();
-            Assert.IsNotNull(hospitalsList);
+            var resoultList = hospital.GetList();
+            Assert.IsNotNull(resoultList);
+        }
+        [TestMethod]
+        public void ReturnHospitalUsersList()
+        {
+            var hospitalUser = new HospitalUserModelCreator();
+            var resoultList = hospitalUser.GetList();
+            Assert.IsNotNull(resoultList);
         }
         [TestMethod]
         public void ReturnSectionModelsList()
         {
             var sectionModel = new SectionModelCreator();
-            var sectionModelsList= sectionModel.GetList();
-            Assert.IsNotNull(sectionModelsList);
+            var resoultList = sectionModel.GetList();
+            Assert.IsNotNull(resoultList);
+        }
+        [TestMethod]
+        public void ReturnUserFunctionsList()
+        {
+            var userFunction = new UserFunctionModelCreator();
+            var resoultList = userFunction.GetList();
+            Assert.IsNotNull(resoultList);
+        }
+        [TestMethod]
+        public void ReturnUserTypesList()
+        {
+            var userType = new UserTypeModelCreator();
+            var resoultList = userType.GetList();
+            Assert.IsNotNull(resoultList);
         }
     }
 }
