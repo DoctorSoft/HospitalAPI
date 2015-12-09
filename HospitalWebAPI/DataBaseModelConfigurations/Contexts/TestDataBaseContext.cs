f53613876dd1fc599ae706933fd3f8d66400c13a
unknown <a.saharchuk@SAHARCHUKA.itransition.corp>
2015-12-09 15:29:55 +0300

    The configurations style of connection strings was changed.

--- HospitalWebAPI/DataBaseModelConfigurations/Contexts/TestDataBaseContext.cs
+++ HospitalWebAPI/DataBaseModelConfigurations/Contexts/TestDataBaseContext.cs
@@ -4,9 +4,23 @@ using DataBaseTools.Interfaces;
 
 namespace DataBaseModelConfigurations.Contexts
 {
+    // Warning: Don't commit this file
+
     public class TestDataBaseContext: AbstractConfiguredContext
     {
-        public TestDataBaseContext() : base("TestHospitalDataBase", new OriginalConfigurationFactory())
+        private const string ArturTomashHomeKey = "TestHospitalDataBase_ArturTomashHome";
+        private const string VaniaHomeKey = "TestHospitalDataBase_VaniaHome";
+        private const string TomashPrivateKey = "TestHospitalDataBase_TomashPrivate";
+        private const string ArturWorkKey = "TestHospitalDataBase_ArturWork";
+        private const string VaniaWorkKey = "TestHospitalDataBase_VaniaWork";
+
+        private const string ArturTomashHomeOriginalKey = "OriginalHospitalDataBase_ArturTomashHome";
+        private const string VaniaHomeOriginalKey = "OriginalHospitalDataBase_VaniaHome";
+        private const string TomashPrivateOriginalKey = "OriginalHospitalDataBase_TomashPrivate";
+        private const string ArturWorkOriginalKey = "OriginalHospitalDataBase_ArturWork";
+        private const string VaniaWorkOriginalKey = "OriginalHospitalDataBase_VaniaWork";
+
+        public TestDataBaseContext() : base(ArturWorkKey, new OriginalConfigurationFactory())
         {
         }
     }
