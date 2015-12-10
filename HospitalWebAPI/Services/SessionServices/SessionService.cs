56de1eb3785039068c89469f89776a246c530a28
Vania <megopixel91@gmail.com>
2015-12-10 22:33:45 +0300

    Add in SessionService check authorization.

--- HospitalWebAPI/Services/SessionServices/SessionService.cs
+++ HospitalWebAPI/Services/SessionServices/SessionService.cs
@@ -1,22 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using RepositoryTools.Interfaces.PrivateInterfaces.FunctionRepositories;
 using RepositoryTools.Interfaces.PrivateInterfaces.UserRepositories;
 using Resources;
 using ServiceModels.ModelTools;
 using ServiceModels.ServiceCommandAnswers.SessionCommandAnswers;
 using ServiceModels.ServiceCommands.SessionCommands;
 using Services.Interfaces.SessionServices;
+using StorageModels.Models.FunctionModels;
 
 namespace Services.SessionServices
 {
     public class SessionService : ISessionService
     {
         private readonly ISessionRepository _sessionRepository;
+        private readonly IUserFunctionRepository _userFunctionRepository;
+        private readonly IFunctionRepository _functionRepository;
 
-        public SessionService(ISessionRepository sessionRepository)
+        public SessionService(ISessionRepository sessionRepository, IUserFunctionRepository userFunctionRepository, IFunctionRepository functionRepository)
         {
             _sessionRepository = sessionRepository;
+            _userFunctionRepository = userFunctionRepository;
+            _functionRepository = functionRepository;
         }
 
         public List<CommandAnswerError> GetAccessDeniedErrors()
@@ -30,11 +36,41 @@ namespace Services.SessionServices
                 }
             };
         }
-
-        public bool CheckPresenceOfToken(Guid? token)
+        private class UserFunctioSelectorModel
         {
-            var session = _sessionRepository.GetModels().Any(model => model.Token == token);
-            return session;
+            public int UserTypeId { get; set; }
+
+            public IEnumerable<GroupFunctionStorageModel> GroupFunctions { get; set; }
+        }
+
+        public bool CheckPresenceOfToken(IsTokenHasAccessToFunctionCommand command)
+        {
+            var currentSession = _sessionRepository.GetModels().FirstOrDefault(model => model.Token == command.Token);
+            List<FunctionStorageModel> functionInTable = new List<FunctionStorageModel>();
+
+            if (currentSession != null && currentSession.IsBlocked)
+            {
+                return false; //current session is blocked
+            }
+       
+            var functionSelectors = ((IDbSet<UserFunctionStorageModel>)_userFunctionRepository.GetModels())
+            .Where(model => model.UserId == currentSession.AccountId)
+            .Select(model => model).ToList();
+
+            foreach (var currentFunctionInTable in command.Functions)
+            {
+                functionInTable = ((IDbSet<FunctionStorageModel>) _functionRepository.GetModels())
+                    .Where(model => model.FunctionIdentityName == currentFunctionInTable)
+                    .Select(model => model).ToList();
+            }
+
+            var result = functionSelectors.Where(n => functionInTable.Any(t => t.Id == n.FunctionId && !t.IsBlocked)).ToList();
+            if (result.Count != command.Functions.Count)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public IsTokenHasAccessToFunctionCommandAnswer IsTokenHasAccessToFunction(IsTokenHasAccessToFunctionCommand command)
@@ -43,7 +79,7 @@ namespace Services.SessionServices
 
             var result = new IsTokenHasAccessToFunctionCommandAnswer
             {
-                HasAccess = CheckPresenceOfToken(command.Token)
+                HasAccess = CheckPresenceOfToken(command)
             };
 
             if (!result.HasAccess)
