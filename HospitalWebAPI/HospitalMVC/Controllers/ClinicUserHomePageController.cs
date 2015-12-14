538491514e606e49c0c467daf5ff71603e155155
Vania <megopixel91@gmail.com>
2015-12-14 22:13:03 +0300

    edit controller for clinic user home page.

--- HospitalWebAPI/HospitalMVC/Controllers/ClinicUserHomePageController.cs
+++ HospitalWebAPI/HospitalMVC/Controllers/ClinicUserHomePageController.cs
@@ -4,18 +4,24 @@ using Enums.Enums;
 using HospitalMVC.Filters;
 using ServiceModels.ServiceCommands.MainPageCommands;
 using Services.Interfaces.MainPageServices;
+using Services.Interfaces.ServiceTools;
+using StorageModels.Models.UserModels;
 
 namespace HospitalMVC.Controllers
 {
     public class ClinicUserHomePageController : Controller
     {
         private readonly IMainPageService _mainPageService;
+        private readonly ITokenManager _tokenManager;
+
         private static bool? _reservationStatus;
         private const string StartTimeRegistration = "10:00";
+        private const string EndTimeRegistration = "19:00";
 
-        public ClinicUserHomePageController(IMainPageService mainPageServices)
+        public ClinicUserHomePageController(IMainPageService mainPageServices, ITokenManager tokenManager)
         {
             _mainPageService = mainPageServices;
+            _tokenManager = tokenManager;
         }
 
         [TokenAuthorizationFilter(FunctionIdentityName.MakeEmptyPlaceReservation)]
@@ -27,15 +33,33 @@ namespace HospitalMVC.Controllers
             {
                CheckReservationStatus();
             }
+
+            var user = _tokenManager.GetUserByToken(command.Token);
+
+            SetViewBagUserName(user);
+            SetViewBagTimeRegistration();
+            
             return View(answer);
         }
 
         public void CheckReservationStatus()
         {
-            var start = TimeSpan.Parse(StartTimeRegistration);
+            var startTime = TimeSpan.Parse(StartTimeRegistration);
+            var endTime = TimeSpan.Parse(StartTimeRegistration);
             var now = DateTime.Now.TimeOfDay;
 
-            _reservationStatus = now >= start;
+            _reservationStatus = now >= startTime && now <= endTime;
+            ViewBag.reservationStatus = _reservationStatus;
+        }
+
+        public void SetViewBagTimeRegistration()
+        {
+            ViewBag.startTimeReservation = StartTimeRegistration;
+            ViewBag.endTimeReservation = EndTimeRegistration;
+        }
+        public void SetViewBagUserName(UserStorageModel user)
+        {
+            ViewBag.UserName = user.Name;
         }
     }
 }
