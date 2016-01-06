c2fa6d36ef22d5b15a513bc9b4c80da2bb636c34
Artur/Tomash Home <megopixel91@gmail.com>
2016-01-07 01:48:40 +0300

    The simple schedule without database data was created and implemented as request to change registration first page.

--- HospitalWebAPI/ServiceModels/ServiceCommandAnswers/HospitalRegistrationsCommandAnswers/GetChangeHospitalRegistrationsPageInformationCommandAnswer.cs
+++ HospitalWebAPI/ServiceModels/ServiceCommandAnswers/HospitalRegistrationsCommandAnswers/GetChangeHospitalRegistrationsPageInformationCommandAnswer.cs
@@ -1,8 +1,11 @@
-﻿using ServiceModels.ModelTools;
+﻿using System.Collections.Generic;
+using ServiceModels.ModelTools;
+using ServiceModels.ServiceCommandAnswers.HospitalRegistrationsCommandAnswers.Entities;
 
 namespace ServiceModels.ServiceCommandAnswers.HospitalRegistrationsCommandAnswers
 {
     public class GetChangeHospitalRegistrationsPageInformationCommandAnswer : AbstractTokenCommandAnswer
     {
+        public List<ScheduleTableItem> Schedule { get; set; }
     }
 }
