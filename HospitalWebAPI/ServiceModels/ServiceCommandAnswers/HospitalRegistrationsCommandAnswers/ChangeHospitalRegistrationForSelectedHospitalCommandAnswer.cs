83ff890bc5f55e1b991ebbc0d28ba402186f437f
Vania <megopixel91@gmail.ru>
2016-01-18 23:06:40 +0300

    add query edit registration.

---
+++ HospitalWebAPI/ServiceModels/ServiceCommandAnswers/HospitalRegistrationsCommandAnswers/ChangeHospitalRegistrationForSelectedHospitalCommandAnswer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ServiceModels.ModelTools;
+using ServiceModels.ServiceCommandAnswers.HospitalRegistrationsCommandAnswers.Entities;
+
+namespace ServiceModels.ServiceCommandAnswers.HospitalRegistrationsCommandAnswers
+{
+    public class ChangeHospitalRegistrationForSelectedHospitalCommandAnswer : AbstractTokenCommand
+    {
+        public List<HospitalRegistrationCountStatisticItem> StatisticItems { get; set; } 
+    }
+}
